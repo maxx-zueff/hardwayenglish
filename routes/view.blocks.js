b4686445589590ab63b88d53e2ecb00081af4ee8
Max Zueff <maxx.zueff@gmail.com>
2018-01-30 21:42:53 +0300

    fix css #1

--- routes/view.blocks.js
+++ routes/view.blocks.js
@@ -1,6 +1,28 @@
 // Dependencies
 const express = require('express');
+const mongoose = require('mongoose');
 const router  = express.Router();
+
+const Group = mongoose.model('Group');
+
+// ------------------------------------------------------------------
+// Middleware
+
+let get_group = function (req, res, next) {
+
+	console.log('middleware start');
+
+	if (!req.user) next();
+
+	Group.findById(req.user.group, function(err, group) {
+		console.log('search ready!');
+
+		if (group.name == 'member') req.group = 'member';
+		if (group.name == 'admin') req.group = 'admin';
+
+		next();
+	});
+};
 
 // ------------------------------------------------------------------
 // Blocks
@@ -9,9 +31,18 @@ router.post('/a', function(req, res) {
 	res.send('AAAAA');
 });
 
-router.post('/', function(req, res) {
-	console.log('HERE!');
-	res.render('blocks/intro');
+router.post('/', get_group, function(req, res, next) {
+
+	if (req.group == 'member') {
+		return res.render('blocks/collections');
+	}
+
+
+	if (req.group == 'admin') {
+		return res.render('blocks/admin/collections');
+	}
+
+	return res.render('blocks/intro');
 });
 
 // ------------------------------------------------------------------
